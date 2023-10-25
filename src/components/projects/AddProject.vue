c0b1ff3f96a079673e342fb829e96fd20a2c42f5
Isaac <isaak_1992@hotmail.es>
2023-10-25 09:38:05 +0200

    style projects

--- src/components/projects/AddProject.vue
+++ src/components/projects/AddProject.vue
@@ -53,7 +53,7 @@ const handleDeleteProject = async(projectName) => {
 <template>
 	<SnackBar :text="message" :openSnackbar="openSnackbar" :error="errorMessage ? true : false" />
 	<v-container class="d-flex justify-center">
-		<v-sheet max-width="400" class="ma-4 w-100">
+		<v-sheet max-width="400" class="ma-0 w-100">
 			<form @submit.prevent>
 				<v-text-field label="Project name" v-model="projectName" :rules="[rules.required]"></v-text-field>
 				<div class="d-flex justify-center">
@@ -63,7 +63,7 @@ const handleDeleteProject = async(projectName) => {
 		</v-sheet>
 	</v-container>
 	<v-container class="d-flex flex-column align-center">
-		<v-sheet v-for="(project, index) in projectStorage.currentCustomerProjects" :key="index" class="d-flex align-center justify-space-between px-2 py-1 my-2 w-100" max-width="200" elevation=1>
+		<v-sheet v-for="(project, index) in projectStorage.currentCustomerProjects" :key="index" class="d-flex align-center justify-space-between px-2 py-1 my-2 w-100" max-width="350" elevation=1>
 			<h3 class="ml-4">{{ project}}</h3>
 			<PrimaryButton variant="plain" icon>
 				<DeleteButton @click="() => handleDeleteProject(project)"/>
