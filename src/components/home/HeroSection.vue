b03390c68dda2842a5a2053e5ef86446b5c33f8a
isaac <isaak_1992@hotmail.es>
2023-11-05 16:29:23 +0100

    test

--- src/components/home/HeroSection.vue
+++ src/components/home/HeroSection.vue
@@ -12,15 +12,15 @@ const { mdAndUp } = useDisplay()
   <v-container class="hero-container">
     <v-row>
       <v-col align="center" class="align-self-center">
-        <h1 class="text-center text-dark">STOP wasting your time counting hours</h1>
+        <h1 class="text-center text-dark">Stop wasting your time counting hours</h1>
         <h4 class="text-dark">Empieza a controlar en qué gastas tú tiempo. Tanto para uso propio como para empresas.</h4>
 
         <div class="my-6">
-          <PrimaryButton text="Empieza Ahora" :to="{name: 'register'}" />
+          <PrimaryButton text="Start Now" :to="{name: 'register'}" />
         </div>
       </v-col>
       <v-col align="center" class="align-self-center">
-        <v-img :width="mdAndUp ? 400 : 300" src="src/assets/img/timings.svg" ></v-img>
+        <v-img :width="mdAndUp ? 400 : 300" src="/src/assets/img/timings.svg" ></v-img>
       </v-col>
     </v-row>
   </v-container>
